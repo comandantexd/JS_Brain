e3a10b76e92cba55768c55c9c5c70af08b0587b6
comandantexd <defaperacm@gmail.com>
2019-01-20 16:58:37 +0100

    dynamic networks.

--- brain.js
+++ brain.js
@@ -1,73 +1,85 @@
 class Neuron {
-    constructor(input1, input2, target) {
-        this.guess = false;
-
-        this.i1 = input1;
-        this.i2 = input2;
-        this.target = target;
-
-        this._g_err_o;
-        this._g_o_pondered;
-
-        this._w1 = Math.random();
-        this._g_pondered_w1;
-        this._g_w1;
-
-        this._w2 = Math.random();
-        this._g_pondered_w2;
-        this._g_w2;
-
-        this._b = 1;
-
-        this._pondered;
-        this._o;
-        this._err;
+    // array of inputs, array of
+    constructor() {
+        this.inputs = [];
+        this.weigths = [];
+        this.net = 0;
+        this.out = 0;
+        this.err = 0;
+        this.bias = 1;
     }
 
-    input(input1, input2, target) {
-        this.i1 = input1;
-        this.i2 = input2;
-        this.target = target;
-    }
+    input(input) {
+        this.inputs = input;
 
-    dendrites() { //ponderes the inputs with the weights
-        this._pondered = (this.i1 * this._w1) + (this.i2 * this._w2) + this._b;
-        this.soma();
-        return;
-    }
-
-    soma() { //applyes the activation function of sigmoid
-        this._o = 1/(1 + (Math.pow(Math.E, -this._pondered)));
-        this.axon();
-        return;
-    }
-
-    axon(){ //prints the output and calculates the total error
-        this._err = 1/2 * Math.pow(this.target - this._o, 2);
-        if (this.guess) {
-            console.log(this._o);
-        } else {
-            if (this._err > 0.1) {
-                this.backpropagation();
-                return;
-            } else {
-                console.log(this._o);
-            }
+        for (let i = 0; i < this.inputs.length; i++) {
+            this.weigths[i] = Math.random();
         }
     }
 
-    backpropagation() {
-        this._g_err_o = -(this.target - this._o);
-        this._g_o_pondered = this._o * (1 - this._o);
-        this._g_pondered_w1 = this.i1;
-        this._g_pondered_w2 = this.i2;
+    process() {
+        //calculating the weighted average
+        this.net = 0;
+        for (let i = 0; i < this.inputs.length; i++) {
+            this.net += this.inputs[i] * this.weigths[i];
+        }
+        this.net += this.bias;
 
-        this._g_w1 = this._g_err_o * this._g_o_pondered * this._g_pondered_w1;
-        this._g_w2 = this._g_err_o * this._g_o_pondered * this._g_pondered_w2;
-
-        this._w1 = this._w1 - 0.5 * this._g_w1;
-        this._w2 = this._w2 - 0.5 * this._g_w2;
-        this.dendrites();
-        return;
+        //Passing the sigmoid activation function to the weighted average
+        this.out = 1/(1 + Math.pow(Math.E, -this.net));
+        return this.out;
     }
+}
+
+//Calculating the error of the neuron
+//this.err = (1/2) * Math.pow(this.target - this.out, 2);
+
+let lay = [2,2,1];
+let ins = [1,1];
+let tar = 1;
+
+class Network {
+    constructor(inputs, layer) {
+        this.netOut = 0;
+
+        //initialization of layers and neurons
+        this.layer = new Array(layer.length);
+        this.out = new Array(layer.length);
+        for (let i = 0; i < layer.length; i++) { //layers
+            this.layer[i] = new Array(layer[i]);
+            this.out[i] = new Array(layer[i]);
+            for (let c = 0; c < layer[i]; c++) { //neurons
+                this.layer[i][c] = new Neuron();
+            }
+        }
+        console.log(this.layer);
+    }
+
+    process(inputs, target) {
+        for (let i = 0; i < this.layer.length; i++) { //layers
+            for (let c = 0; c < this.layer[i].length; c++) { //neurons
+
+                if (i == 0) {
+                    this.layer[i][c].input(inputs)
+                    this.out[i][c] = this.layer[i][c].process();
+                    console.log(`Input for layer ${i} y neurona ${c}: ${inputs}`)
+                } else {
+                    this.layer[i][c].input(this.out[i - 1]);
+                    this.out[i][c] = this.layer[i][c].process();
+                    console.log(`Input for layer ${i} y neurona ${c}: ${this.out[i - 1]}`)
+
+                    if (i == this.layer.length - 1) { //checks if is last layer
+                        this.netOut = this.out[i][c];
+                    }
+                }
+            }
+        }
+        console.log(this.out);
+    }
+    // backpropagation() {
+    //     for (let i = 0; i < this.inputs.length; i++) {
+    //         this.gradient = -(this.target - this.out) * this.out * (1 - this.out) * this.inputs[i];
+    //         this.weigths[i] -= 0.5 * this.gradient;
+    //     }
+    // }
 }
